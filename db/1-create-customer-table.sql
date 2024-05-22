2fe1c590830458e2ed9f8f7c68583a7bbbb50f1d
icarodamiani <icarodamiani@gmail.com>
2024-05-22 04:04:48 -0300

    Development (#5)

--- db/1-create-customer-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE customer
-(
-    id    serial primary key,
-    name        VARCHAR(40) not null,
-    email        VARCHAR(40) not null UNIQUE,
-    vat        VARCHAR(40) not null UNIQUE,
-    phone        VARCHAR(40) null
-);
